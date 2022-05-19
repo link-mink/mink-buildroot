d5ed6a591736535b21392ac160326de57d484958
Damir Franusic <damir.franusic@gmail.com>
2022-05-19 14:00:20 +0200

    feat: add missing configure switches

--- package/link_mink_core/link_mink_core.mk
+++ package/link_mink_core/link_mink_core.mk
@@ -17,14 +17,19 @@ LINK_MINK_CORE_CONF_OPTS = \
         --with-boost-libdir=$(STAGING_DIR)/usr/lib \
 	$(if $(BR2_PACKAGE_LINK_MINK_CORE_SYSAGENT),--enable-sysagent) \
 	$(if $(BR2_PACKAGE_LINK_MINK_CORE_SYSAGENT_SYSLOG),--enable-syslog) \
-	$(if $(BR2_PACKAGE_LINK_MINK_CORE_SYSAGENT_OPENWRT),--enable-openwrt) \
 	$(if $(BR2_PACKAGE_LINK_MINK_CORE_SYSAGENT_LUA),--enable-lua) \
 	$(if $(BR2_PACKAGE_LINK_MINK_CORE_SYSAGENT_MQTT),--enable-mqtt) \
 	$(if $(BR2_PACKAGE_LINK_MINK_CORE_SYSAGENT_NDPI),--enable-ndpi) \
+	$(if $(BR2_PACKAGE_LINK_MINK_CORE_SYSAGENT_UNIX),--enable-unix) \
+	$(if $(BR2_PACKAGE_LINK_MINK_CORE_SYSAGENT_CGROUP2),--enable-cgroup2) \
+	$(if $(BR2_PACKAGE_LINK_MINK_CORE_SYSAGENT_SYSTEMD),--enable-systemd) \
+	$(if $(BR2_PACKAGE_LINK_MINK_CORE_SYSAGENT_CFG_HNDLR),--enable-config-handler) \
+	$(if $(BR2_PACKAGE_LINK_MINK_CORE_SYSAGENT_MODBUS),--enable-modbus) \
 	$(if $(BR2_PACKAGE_LINK_MINK_CORE_GDTTRAPC),--enable-gdttrapc) \
 	$(if $(BR2_PACKAGE_LINK_MINK_CORE_CODEGEN),--enable-codegen) \
 	$(if $(BR2_PACKAGE_LINK_MINK_CORE_JRPCD),--enable-jrpc --enable-openssl) \
 	$(if $(BR2_PACKAGE_LINK_MINK_CORE_GRPCD),--enable-grpc) \
+	$(if $(BR2_PACKAGE_LINK_MINK_CORE_CFGD),--enable-configd) \
 	$(if $(BR2_PACKAGE_LINK_MINK_CORE_JRPCD_WS),--enable-plain-ws) \
 	$(if $(BR2_PACKAGE_LINK_MINK_CORE_JRPCD_TLS12),--enable-tlsv12) \
 	$(if $(BR2_PACKAGE_LINK_MINK_CORE_JRPCD_SS),--enable-ws-single-session)
