3dbb7f6fc3c96c291b80fbd97e5d283d6a3cf5ef
Damir Franusic <damir.franusic@gmail.com>
2022-06-17 13:03:48 +0200

    feat: generate custom os-release file

--- board/raspberrypi2_mink/post-build.sh
+++ board/raspberrypi2_mink/post-build.sh
@@ -18,7 +18,14 @@ find ${TARGET_DIR}/usr/lib/mink -type l -exec rm {} \;
 test -f ${TARGET_DIR}/usr/lib/mink/plg_sysagent_system.so.1.0.0 && \
     { mv ${TARGET_DIR}/usr/lib/mink/plg_sysagent_system.so.1.0.0 \
          ${TARGET_DIR}/usr/lib/mink/00-plg_sysagent_system.so.1.0.0; }
-
+# update os-release
+MINK_OS_VER=$(cd "$CONFIG_DIR/.." && git rev-parse HEAD)
+sed -i -e "s/\(NAME=\)\(.*\)/\1mINK_OS/g" ${TARGET_DIR}/etc/os-release
+sed -i -e "s/\(VERSION=\)\(.*\)/\1${MINK_OS_VER}/g" ${TARGET_DIR}/etc/os-release
+sed -i -e "s/\(ID=\)\(.*\)/\1mink_os/g" ${TARGET_DIR}/etc/os-release
+sed -i -e "s/\(VERSION_ID=\)\(.*\)/\1${MINK_OS_VER}/g" ${TARGET_DIR}/etc/os-release
+sed -i -e "s/\(PRETTY_NAME=\)\(.*\)/\1\"mINK OS\"/g" ${TARGET_DIR}/etc/os-release
+# info
 echo "==============================================="
 echo "           ___ _   _ _  __   ___  ____"
 echo " _ __ ___ |_ _| \ | | |/ /  / _ \/ ___|"
@@ -26,5 +33,6 @@ echo "| '_ \` _ \ | ||  \| | ' /  | | | \\___ \\"
 echo "| | | | | || || |\  | . \  | |_| |___) |"
 echo "|_| |_| |_|___|_| \_|_|\_\  \___/|____/"
 echo "==============================================="
-echo "device id: $UUID"
+echo "Version: $MINK_OS_VER"
+echo "Device UUID: $UUID"
 echo "==============================================="
