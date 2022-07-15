6b972675894a2e4ec4abb8e3cd992d40f28a4f65
Damir Franusic <damir.franusic@gmail.com>
2022-07-15 15:07:12 +0200

    feat: use mink_uuid from uboot (ESPRESSObin)

--- board/espressobin_mink/overlay/usr/share/mink/mink_os_firstboot.sh
+++ board/espressobin_mink/overlay/usr/share/mink/mink_os_firstboot.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
-MINK_UUID=$(cat /usr/share/lua/5.1/device.lua|sed -n -e '2p'|sed -e 's/.*id = "\(.*\)"/\1/g')
-fw_setenv mink_uuid $MINK_UUID
+# get from uboot env
+MINK_UUID=$(fw_printenv mink_uuid|sed -e 's/.*=//g')
+if [ -z "$MINK_UUID" ]
+then
+    # set uboot env from device.lua
+    MINK_UUID=$(cat /usr/share/lua/5.1/device.lua|sed -n -e '2p'|sed -e 's/.*id = "\(.*\)"/\1/g')
+    fw_setenv mink_uuid $MINK_UUID
+else
+    # set mink uuid from uboot env
+    sed -i -e "s/id = \".*\"/id = \"$MINK_UUID\"/g" /usr/share/lua/5.1/device.lua
+    sed -i -e "s#\(mink/\).*\(/log.*\)#\1$MINK_UUID\2#g" /usr/lib/systemd/system/mink_log.service
+    cat /etc/mink/plg_cfg.json.tmpl | sed -e "s/@ID@/$MINK_UUID/g" > /etc/mink/plg_cfg.json
+    # reload
+    systemctl daemon-reload
+    systemctl restart mink_log
+    systemctl stop mosquitto
+    systemctl stop mink_sysagent
+    systemctl stop mink_rt
+    systemctl start mosquitto
+    systemctl start mink_rt
+    systemctl start mink_sysagent
+fi
